e2365dc45db0c0f6fb01b35e7cd653eea71ad0d5
shroukwagdysayed <shroukwagdy05@gmail.com>
2024-07-12 18:45:07 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const BirthDayCardApp());
+}
+
+class BirthDayCardApp extends StatelessWidget {
+  const BirthDayCardApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
